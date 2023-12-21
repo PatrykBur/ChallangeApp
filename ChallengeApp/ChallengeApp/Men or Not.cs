1b93260154a1f72c15c84662c4a4d16c1d98b43d
PatrykBur <Patrykburkaczewski@gmail.com>
2023-12-21 02:34:27 +0100

    zadanie 1

---
+++ ChallengeApp/ChallengeApp/Men or Not.cs
@@ -0,0 +1,33 @@
+﻿var name = "Ewa";
+bool men = true;
+var wiek = 30;
+
+if (men == false)
+{
+    if (wiek >= 30)
+    {
+        if (name == "Ewa")
+        {
+            Console.WriteLine("Ewa lat 30");
+        }
+        else
+        {
+            Console.WriteLine("Kobieta 30 lat");
+        }
+    }
+    else
+    {
+        Console.WriteLine(" ");
+    }
+}
+else
+{
+    if (wiek >= 18)
+    {
+        Console.WriteLine(" ");
+    }
+    else
+    {
+    Console.WriteLine("niepelnoletni meszczyzna");
+    }
+}
