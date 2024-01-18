0edf6fb1a263c2115f7896bc5123982e31310443
PatrykBur <Patrykburkaczewski@gmail.com>
2024-01-18 10:48:11 +0100

    Dzien 5

---
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+int number = 125457746;
+int[] table = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+Console.WriteLine("wynik dla liczby " + number);
+for (int i = 0; i < table.Length; i++)
+{
+    int numberofdigits = 0;
+    int digits = table[i];
+    int pozycjaCyfry = number.ToString().IndexOf(digits.ToString());
+
+    while (pozycjaCyfry != -1)
+    {
+        numberofdigits++;
+        pozycjaCyfry = number.ToString().IndexOf(digits.ToString(), pozycjaCyfry + 1);
+    }
+    Console.WriteLine(table[i] + "->" + numberofdigits);
+}
